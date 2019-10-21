408a517a52e6100208ae64bb6c4e6e4c7ac30048
Sejin Kim <froydroyce@gmail.com>
2019-10-21 15:41:04 -0600

    Create graphql schema with questions query and question model
    Co-Authored-By: Djavan Munroe <djavanm@users.noreply.github.com>

---
+++ graphql/schema.js
@@ -0,0 +1,16 @@
+var { buildSchema } = require('graphql')
+
+module.exports = buildSchema(`
+  type Question {
+    id: Int
+    body: String
+    active: Boolean
+  },
+  type Query {
+    questions(
+      id: Int
+      body: String
+      active: Boolean
+    ): [Question]
+  }
+`)
