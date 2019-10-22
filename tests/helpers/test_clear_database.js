565f183adbcb9424072983efc4193d34deec92dc
Djavan Munroe <djavanm24@gmail.com>
2019-10-22 17:32:21 -0600

    Update test_clear_database.js
    Updated clear database functions to destroy user and interview table in database.
    
    Co-Authored-By: Sejin Kim <froydroyce@users.noreply.github.com>

--- tests/helpers/test_clear_database.js
+++ tests/helpers/test_clear_database.js
@@ -1,5 +1,9 @@
 const Question = require('../../models').Question;
+const Interview = require('../../models').Interview;
+const User = require('../../models').User;
 
 module.exports = async function cleanup() {
   await Question.destroy({ where: {} })
+  await Interview.destroy({ where: {} })
+  await User.destroy({ where: {} })
 }
