2d6bcf894e4a1b5fc96676a0ae540b0158186e70
Djavan Munroe <djavanm24@gmail.com>
2019-10-22 17:35:43 -0600

    Update resolvers.js
    Add createInterview resolver to create records for an interview, and interviewUser records.
    
    Co-Authored-By: Sejin Kim <froydroyce@users.noreply.github.com>

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,7 @@
 const Question = require('../models').Question;
+const Interview = require('../models').Interview;
+const InterviewUser = require('../models').InterviewUser;
+const User = require('../models').User;
 const Sequelize = require('sequelize');
 
 module.exports = {
@@ -28,5 +31,32 @@ module.exports = {
     .then(updatedQuestion => {
       return updatedQuestion[1][0]
     })
+  },
+
+  createInterview: async ({studentId, interviewerId}) => {
+    return await Interview.create()
+      .then(async interview => {
+        await InterviewUser.create({
+          interviewId: interview.id,
+          userId: studentId
+        })
+        await InterviewUser.create({
+          interviewId: interview.id,
+          userId: interviewerId
+        })
+        return interview;
+      })
+      .then(interview => {
+        return Interview.findOne({
+          where: { id: interview.id },
+          include: [{
+            model: User,
+            as: 'users',
+            through: {
+              attributes: []
+            }
+          }]
+        })
+      })
   }
 };
