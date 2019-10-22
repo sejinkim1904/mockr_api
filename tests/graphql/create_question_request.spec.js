0d710db414dc7410f24e02fe81ccf436415ea589
Sejin Kim <froydroyce@gmail.com>
2019-10-22 11:38:03 -0600

    Update body for request

--- tests/graphql/create_question_request.spec.js
+++ tests/graphql/create_question_request.spec.js
@@ -11,15 +11,23 @@ describe('Mockr API', () => {
     });
 
     test('It returns the created question', async () => {
+      let body = "Do you like writing tests?"
+      let reqBody = {
+        "query": `mutation {
+          addQuestion(body: "${body}")
+          {id,body,active}
+        }`
+      };
+
       return request(app)
-        .get('/graphql?mutation={addQuestion(body:"Do you like making tests?")}}')
+        .post('/graphql')
+        .send(reqBody)
         .then(response => {
+          expect(response.status).toBe(200)
           console.log(response.body)
-          expect(response.status).toBe(201)
-          expect(response.body.data.questions.length).toBe(1)
-          expect(Object.keys(response.body.data.questions[0])).toContain("id")
-          expect(Object.keys(response.body.data.questions[0])).toContain("body")
-          expect(Object.keys(response.body.data.questions[0])).toContain("active")
+          expect(Object.keys(response.body.data.addQuestion)).toContain("id")
+          expect(Object.keys(response.body.data.addQuestion)).toContain("body")
+          expect(Object.keys(response.body.data.addQuestion)).toContain("active")
         })
     })
   })
