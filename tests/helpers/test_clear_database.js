3cd5d6b748d98cd11e5da75df98d49114cde28e8
Sejin Kim <froydroyce@gmail.com>
2019-10-23 12:05:56 -0600

    Add function to destroy InterviewUser records before tests
    Co-Authored-By: Djavan Munroe <djavanm@users.noreply.github.com>

--- tests/helpers/test_clear_database.js
+++ tests/helpers/test_clear_database.js
@@ -1,9 +1,11 @@
 const Question = require('../../models').Question;
 const Interview = require('../../models').Interview;
+const InterviewUser = require('../../models').InterviewUser;
 const User = require('../../models').User;
 
 module.exports = async function cleanup() {
   await Question.destroy({ where: {} })
+  await InterviewUser.destroy({ where: {} })
   await Interview.destroy({ where: {} })
   await User.destroy({ where: {} })
 }
